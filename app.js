a70db4487e6cdfa573165a25b727214d8e3f51a6
Hifi58 <gillet.tony@gmail.com>
2022-08-29 16:47:19 +0200

    hotfix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('*', (rep, res) =>{
-    res.status(200).json('Zeub')
+    res.status(200).json('Hello_Docker')
 })
 app.listen(80)
